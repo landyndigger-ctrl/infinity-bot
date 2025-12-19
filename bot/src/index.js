d0f52cb4193c1feb54013af77c78649e6c1bc344
landyndigger-ctrl <landyndigger@gmail.com>
2025-12-18 17:29:19 -0800

    Delete bot/src directory

--- bot/src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const client = require('./client');
-const connectDB = require('./database/mongo');
-
-
-(async () => {
-await connectDB();
-await client.login(process.env.DISCORD_TOKEN);
-})();
